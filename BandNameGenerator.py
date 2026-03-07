afcace62c6ba0d5fd3f3f436054ccf9b6d3ad4a7
JJCsC110AB <Jas2256355@maricopa.edu>
2026-03-07 10:11:57 -0700

    First day

---
+++ BandNameGenerator.py
@@ -0,0 +1,9 @@
+class BandNameGenerator:
+
+    print("Welcome to the Band Name Generator \nWhats the name of the City you grew up in?")
+    
+    City = input()
+    print("Whats your pets name?")
+    Pet = input()
+    print("Your band name is:")
+    print(f"{City} {Pet}")
